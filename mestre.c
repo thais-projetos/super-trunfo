99a3fe95160334f4fc70e070eb1cec4cb78dd940
Thaís Cristina <tcandradesantoss@gmail.com>
2025-03-11 21:00:22 -0300

    Update mestre.c

--- mestre.c
+++ mestre.c
@@ -3,7 +3,7 @@
 // Desafio Super Trunfo - Nível Mestre - Batalha Final //
 
 int main() {
-    // Carta 1
+    // Carta 1//
     char estado1 = 'A';
     char codigo1[] = "A01";
     char nomeCidade1[] = "Curitiba";
@@ -15,7 +15,7 @@ int main() {
     float pibPerCapita1 = pib1 / populacao1;
     float superPoder1 = populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1 / densidade1);
     
-    // Carta 2
+    // Carta 2//
     char estado2 = 'B';
     char codigo2[] = "B02";
     char nomeCidade2[] = "Belo Horizonte";
@@ -27,7 +27,7 @@ int main() {
     float pibPerCapita2 = pib2 / populacao2;
     float superPoder2 = populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1 / densidade2);
     
-    // Comparação de Atributos
+    // Comparação de Atributos//
     printf("\nComparacao de Cartas:\n\n");
     printf("Populacao: Carta %d venceu (%lu)\n", populacao1 > populacao2 ? 1 : 2, populacao1 > populacao2 ? populacao1 : populacao2);
     printf("Area: Carta %d venceu (%.2f km²)\n", area1 > area2 ? 1 : 2, area1 > area2 ? area1 : area2);
@@ -37,7 +37,7 @@ int main() {
     printf("PIB per Capita: Carta %d venceu (%.2f reais)\n", pibPerCapita1 > pibPerCapita2 ? 1 : 2, pibPerCapita1 > pibPerCapita2 ? pibPerCapita1 : pibPerCapita2);
     printf("Super Poder: Carta %d venceu (%.2f)\n", superPoder1 > superPoder2 ? 1 : 2, superPoder1 > superPoder2 ? superPoder1 : superPoder2);
     
-    // Carta Vencedora Geral
+    // Carta Vencedora Geral//
     int vitoriasCarta1 = (populacao1 > populacao2) + (area1 > area2) + (pib1 > pib2) + (pontosTuristicos1 > pontosTuristicos2) + (densidade1 < densidade2) + (pibPerCapita1 > pibPerCapita2) + (superPoder1 > superPoder2);
     int vitoriasCarta2 = 7 - vitoriasCarta1;
     
